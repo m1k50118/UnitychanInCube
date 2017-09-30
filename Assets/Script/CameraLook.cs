fb10273b2c164ad2cd485019b197af33d556f822
佐藤真 <ac.backfill1539@gmail.com>
2017-09-30 18:22:51 +0900

    Playerのスクリプトを追加

--- Assets/Script/CameraLook.cs
+++ Assets/Script/CameraLook.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour {
 
+    public GameObject playerLook;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,7 +15,22 @@ public class CameraLook : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
+            //playerLook.transform.position = new Vector3();  
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+            
 
         }
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+            
+
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+           
+
+		}
     }
 }
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Vector2 startPos;
+    private Vector2 endPos;
+    float speed = 0;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 direct = Input.mousePosition;
+            this.speed = direct.x - startPos.x;
+            player.transform.position += new Vector3(speed * 0.001f, 0, 0);
+
+        }
+    }
+}
